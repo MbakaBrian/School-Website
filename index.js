1aab994573416e3e9bec8b808d3099f121d4de1f
MbakaBrian <brianmbakanjc@gmail.com>
2024-03-23 10:24:01 +0300

    4th commit

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuBtn = document.querySelector('.menu-btn');
+    const menuBtn = document.getElementById('menuBtn');
     const navLinks = document.querySelector('.nav-links');
 
     menuBtn.addEventListener('click', () => {
         navLinks.classList.toggle('active');
     });
 });
-
-
